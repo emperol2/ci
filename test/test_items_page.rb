b1799843ddc5d04838a179a64b5e9d6c2bdb2e11
nuttapon <emperol2@gmail.com>
2015-01-27 19:27:40 +0700

    added youmayalsolike test case

--- test/test_items_page.rb
+++ test/test_items_page.rb
@@ -57,6 +57,48 @@ module Test
 
     end
 
+    def test_visible_image_youmayalsolike
+      ary = Array.new
+      itemsArray = Array.new
+      nodeURL = 'http://www.bk.com/node/'
+
+      cagetoryList = @driver.find_elements(:css, '.food-category a')
+      cagetoryList.each do |c|
+        ary.push(c.attribute('href'))
+      end
+
+      ary.each do |aa|
+        @driver.navigate.to(aa)
+        findItemLinks = @driver.find_elements(:css, 'div.col-xs-6')
+        findItemLinks.each do |f|
+          #p f.attribute('data-product-id')
+          itemsArray.push(nodeURL+f.attribute('data-product-id'))
+          #p itemsArray.size()
+        end
+      end
+
+      itemsArray.uniq.each do |a|
+        @driver.navigate.to(a)
+        #p a
+        imgList = @driver.find_elements(:tag_name, 'img')
+        imgList.each do |i|
+          #puts i.attribute('src')
+          # if i.attribute('typeof') != nil
+          #   p i.attribute('src')
+          #   res = Net::HTTP.get_response(URI(i.attribute('src')))
+          #   assert_equal('200', res.code, "This is error #{i.attribute('src')}")
+          # end
+          if i.attribute('src').include?('square_thumbnail') == true && i.attribute('typeof') == nil
+            p i.attribute('src')
+            res = Net::HTTP.get_response(URI(i.attribute('src')))
+            assert_equal('200', res.code, "This is error #{i.attribute('src')}")
+          end
+          p "\n"
+        end
+      end
+
+    end
+
 
 
 
